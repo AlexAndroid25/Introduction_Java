026870c087e53271bae0af158978868ecc176747
AlexTutor <alexprogramming25@gmail.com>
2021-02-12 17:56:16 -0800

    [Update]

--- src/Lesson1/Lesson1.java
+++ src/Lesson1/Lesson1.java
@@ -5,5 +5,6 @@ public class Lesson1 {
     public static void main(String args[]) {
         /* Hello guys */
         /* Karim & Nasser */
+        System.out.println("Hello World First Program");
     }
 }
